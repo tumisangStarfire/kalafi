228f49e86132ab1e7f7c4f604eb9bb9632671a75
tumi14 <tumisang.alphains@gmail.com>
2020-02-19 16:57:50 +0200

    web

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import router from './router/router';
 import { loggerMiddleware } from './middleware/loggerMiddleware';
 var path = require('path');
 const dotenv = require('dotenv');
+process.env.PWD = process.cwd()
 dotenv.config()
 export class App {
 
@@ -24,7 +25,7 @@ export class App {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(loggerMiddleware);
-        this.app.use(express.static(path.join(__dirname + 'public')));
+        this.app.use(express.static(process.env.PWD + './public'));
         //this.app.set('views', path.join(__dirname + '/public/web'));
         //this.app.engine('html', require('ejs').renderFile);
         //this.app.set('view engine', 'html');
@@ -38,6 +39,9 @@ export class App {
         this.app.use('/v1/api', router);
         this.app.get("/", function (req, res) {
             res.sendFile('./public/web/views/index.html', { root: __dirname });
+        });
+        this.app.get('/legal/privacy-policy', function (req, res) {
+            res.sendFile('./public/web/views/privacy_policy.html', { root: __dirname });
         })
     }
 
