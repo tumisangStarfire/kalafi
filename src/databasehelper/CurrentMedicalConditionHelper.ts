0d38c84c882f4a2f2a3bc84772c30897e152dc49
Tumisang <mlamar347@gmail.com>
2020-05-22 18:40:06 +0200

    changes

--- src/databasehelper/CurrentMedicalConditionHelper.ts
+++ src/databasehelper/CurrentMedicalConditionHelper.ts
@@ -3,33 +3,34 @@ import { JsonResponseInterface } from "../interfaces/JsonResponseInterface";
 import { MongoHelper } from "../database/MongoHelper";
 
 export class CurrentMedicalConditionHelper {
-   
+
 
     static create = async (currentMedicalCondition: CurrentMedicalCondition, callback) => {
         try {
             const query =  MongoHelper.client.db('Mooki_Development').collection('currentmedicalcondition');
             var result = query.insertOne(currentMedicalCondition, function (err, res) {
                 if (err) {
-                    console.log(err); 
-                    var jsonRes : JsonResponseInterface; 
-                    jsonRes.status = 'failed';  
-                    jsonRes.message ='failed to add medical information'; 
-                    jsonRes.data =err; 
-                    jsonRes.code =404;
-                }   
-                var jsonRes : JsonResponseInterface; 
-                    jsonRes.status = 'success';  
-                    jsonRes.message ='medical information added succesfully'; 
-                    jsonRes.data =res.insertedId; 
-                    jsonRes.code =200;
+                    console.log(err);
+                    var jsonRes : JsonResponseInterface ={
+                        status : 'failed',
+                        message :'failed to add medical information',
+                        data :err,
+                        code :404,
+                    };
+                }
+                var jsonRes : JsonResponseInterface = {
+                    status : 'success',
+                    message :'medical information added succesfully',
+                    data :res.insertedId,
+                    code :200,
+                }
 
-                console.log(res);
                 return callback(jsonRes);
             });
         } catch (error) {
             console.log(error);
         }
-    }    
+    }
 
     static remove = async (storageId:string, callback) => {
         try {
@@ -37,18 +38,18 @@ export class CurrentMedicalConditionHelper {
             var deleteParams = { _id: storageId };
             var result = query.deleteOne(deleteParams, function (err, res) {
                 if (err) {
-                    console.log(err); 
-                    var jsonRes : JsonResponseInterface; 
-                    jsonRes.status = 'failed';  
-                    jsonRes.message ='failed to delete medical information'; 
-                    jsonRes.data =err; 
+                    console.log(err);
+                    var jsonRes : JsonResponseInterface;
+                    jsonRes.status = 'failed';
+                    jsonRes.message ='failed to delete medical information';
+                    jsonRes.data =err;
                     jsonRes.code =404;
                 }
                 console.log(res);
-                var jsonRes : JsonResponseInterface; 
-                    jsonRes.status = 'success';  
-                    jsonRes.message ='medical information deleted succesfully'; 
-                    jsonRes.data =res.deletedCount; 
+                var jsonRes : JsonResponseInterface;
+                    jsonRes.status = 'success';
+                    jsonRes.message ='medical information deleted succesfully';
+                    jsonRes.data =res.deletedCount;
                     jsonRes.code =201;
                 return callback(jsonRes);
             });
@@ -66,25 +67,25 @@ export class CurrentMedicalConditionHelper {
                 if (err) {
                     var JsonResponse = {
                         status: 'failed',
-                        message: 'failed to fetch user medication information', 
+                        message: 'failed to fetch user medication information',
                         data: {},
                         code: 404
-                    }   
+                    }
                      console.log(err);
-                    var jsonres : JsonResponseInterface = JsonResponse ; 
+                    var jsonres : JsonResponseInterface = JsonResponse ;
                     return callback(jsonres);
-                }  
+                }
 
-                //array of medical data 
+                //array of medical data
                 var currentmedication: Array<CurrentMedicalCondition>;
                 currentmedication = res;
-               // console.log(currentmedication); 
-                var jsonres : JsonResponseInterface; 
+               // console.log(currentmedication);
+                var jsonres : JsonResponseInterface;
                 jsonres.status = 'success';
-                jsonres.message = 'user medication data has been fetched'; 
-                jsonres.data = currentmedication; 
-                jsonres.code =200; 
-                
+                jsonres.message = 'user medication data has been fetched';
+                jsonres.data = currentmedication;
+                jsonres.code =200;
+
                 return callback(jsonres);
             });
 
--- src/databasehelper/HealthFacilityHelper.ts
+++ src/databasehelper/HealthFacilityHelper.ts
@@ -11,25 +11,27 @@ export class HealthFacilityHelper {
             const query = await MongoHelper.client.db('Mooki_Development').collection('healthfacilities');
             query.find({}).toArray(function (err, res) {
                 if (err) {
-                    var JsonResponse = {
+
+                     console.log(err);
+                    var jsonres : JsonResponseInterface = {
                         status: 'failed',
-                        message: 'failed to fetch health facility data', 
+                        message: 'failed to fetch health facility data',
                         data: {},
                         code: 404
-                    }   
-                     console.log(err);
-                    var jsonres : JsonResponseInterface = JsonResponse ; 
+                    }    ;
                     return callback(jsonres);
                 }
                 var healthFacility: Array<HealthFacility>;
-                healthFacility = res; 
+                healthFacility = res;
 
-                var jsonres : JsonResponseInterface; 
-                jsonres.status = 'success';
-                jsonres.message = 'Health facility data has been fetched'; 
-                jsonres.data = healthFacility; 
-                jsonres.code =200;
-                return callback(jsonres);
+               /* var jsonres : JsonResponseInterface = {
+                    status: 'success',
+                    message: 'Health facility data has been fetched',
+                    data:healthFacility,
+                    code: 200
+                } ;*/
+
+                return callback(healthFacility);
             });
         } catch (error) {
             console.log(error);
