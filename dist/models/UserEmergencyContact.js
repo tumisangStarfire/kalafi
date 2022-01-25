5f771e69a1b9ad67ff394a59e3259685585e129d
tumisangStarfire <tumisangmogotsioutlook.com>
2022-01-25 20:08:11 +0200

    build

--- dist/models/UserEmergencyContact.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserEmergencyContact extends Document {
-    constructor(firstName, cellphone, relation) {
-        super();
-    }
-}
-exports.UserEmergencyContact = UserEmergencyContact;
-//# sourceMappingURL=UserEmergencyContact.js.map
--- dist/router/authrouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import registerUser from 'controller/auth/RegisterController';
-import login from 'controller/auth/LoginController';
-const router = Router();
-router.route('/api/register').post(registerUser);
-router.route('/api/login').post(login);
-export default router;
-//# sourceMappingURL=authrouter.js.map
